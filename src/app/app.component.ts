05b6a733431413d08f456b655451b41a12066f74
Geraldo Pereira da Costa Neto <gera.cost18@gmail.com>
2024-09-30 20:43:07 -0300

    playing with promises and observables

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title = 'AngularPlayground';
+export class AppComponent implements OnInit {
+  inputProduct = '';
+
+  private timer = new Promise<string>((resolver) => {
+    setTimeout(() => {
+      resolver('RESOLVIDO');
+    }, 5000);
+  });
+
+  private timer$ = new Observable<string>((sub) => {
+    setTimeout(() => {
+      sub.next('RESOLVIDO');
+    }, 5000);
+  });
+
+  ngOnInit(): void {
+    this.timer.then((value) => console.log('value: ', value));
+    this.timer$.subscribe((value) => console.log('value: ', value));
+  }
+
+  addProduct() {}
 }
