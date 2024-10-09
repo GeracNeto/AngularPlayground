7f7b0a176b71e82cbe35bea43431bcd81d770984
Geraldo Pereira da Costa Neto <gera.cost18@gmail.com>
2024-10-09 19:49:39 -0300

    simplify code using AsyncPipe

--- src/app/music/music.component.ts
+++ src/app/music/music.component.ts
@@ -1,23 +1,25 @@
 import { Component } from '@angular/core';
 import { MusicService } from './music.service';
 import { Music } from './music.model';
+import { Observable } from 'rxjs';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-music',
   standalone: true,
-  imports: [],
+  imports: [AsyncPipe],
   styleUrl: './music.component.scss',
   templateUrl: './music.component.html'
 })
 export class MusicComponent {
 
-  musics: Music[] = [];
+  musics$ = new Observable<Music[]>();
 
   constructor(private musicService: MusicService) {
     this.getRegisteredMusics();
   }
 
   getRegisteredMusics() {
-    this.musicService.getMusics().subscribe(musics => this.musics = musics);
+    this.musics$ = this.musicService.getMusics();
   }
 }
