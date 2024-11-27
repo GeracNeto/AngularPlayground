cb517ba868c10547d90734752a7e82131f2f3702
Geraldo Pereira da Costa Neto <gera.cost18@gmail.com>
2024-11-27 19:25:44 -0300

    delete unused code to start NgRx classes, updates README

--- README.md
+++ README.md
@@ -2,10 +2,6 @@
 
 # <img src="https://angular.io/assets/images/logos/angular/angular.png" alt="Angular Icon" width="50"/>  Angular Playground
 
-## Classes
+## NgRx -> https://www.youtube.com/watch?v=uB2Wk9zS6H0&list=PLWgD0gfm500GdPyP4ut5RvW7hkjKlKKaQ
 
-- ANGULAR OBSERVABLES INICIANTES GUIA PRÁTICO
-  - https://www.youtube.com/watch?v=8ZKpJ5A5Ru4&t=1271s
-
-- ANGULAR: CONSUMINDO API ATUALIZADO 2023
-  - https://www.youtube.com/watch?v=rjrQpMYtTUw&t=312s
+### Notas
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,29 +1,11 @@
-import { Component, Inject, OnInit } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { RouterOutlet } from '@angular/router';
-import { CarrinhoComponent } from './compras/carrinho.component';
-import { CarrinhoService } from './compras/carrinho.service';
-import { MusicComponent } from './music/music.component';
-import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FormsModule, CarrinhoComponent, MusicComponent],
+  imports: [],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent implements OnInit {
-  inputProduct = '';
-  mostrarContador = true;
-
-  constructor(private carrinhoService: CarrinhoService) { }
-
-  ngOnInit(): void { }
-
-  addProduct() {
-    this.carrinhoService.adicionarProduto(this.inputProduct);
-
-    this.inputProduct = '';
-  }
+export class AppComponent {
 }
