564782628b801c75365f713b6fa8ba0b07765ec4
Geraldo Pereira da Costa Neto <gera.cost18@gmail.com>
2024-11-27 19:59:29 -0300

    creates initial template with bootstrap to work with NgRx

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { CommonModule } from '@angular/common';
+import { AddIngredientComponent } from "./add-ingredient/add-ingredient.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ShoppingListComponent, AddIngredientComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
+  ingredients?: any[] = [
+    { name: 'Apples', amount: 5 },
+    { name: 'Tomatoes', amount: 10 }
+  ]
 }
---
+++ src/app/shopping-list/shopping-list.component.ts
@@ -0,0 +1,14 @@
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-shopping-list',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './shopping-list.component.html',
+  styleUrl: './shopping-list.component.scss'
+})
+export class ShoppingListComponent {
+  @Input() ingredients?: any[];
+
+}
